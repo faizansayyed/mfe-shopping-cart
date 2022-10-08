7422da34fca742fb9b9ae0b8f2b99cb2cc7314da
Faizan Sayyed <faizansayyed36@gmail.com>
2022-10-08 17:41:57 +0530

    initial app setup added header and footer and exposed to remotes

---
+++ home/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+            <div className="flex">
+                <div className="flex-grow flex">
+                    Header Component
+                </div>
+            </div>
+        </div>
+    );
+}
